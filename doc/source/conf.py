1559ffe9c1be85c18618b8117e655b322e815472
Guenther Beulen <guenther.beulen@web.de>
2020-09-21 10:19:01 +0200

    Make it executabel by RTD.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -73,8 +73,8 @@ html_static_path = ['_static']
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    input_dir = '../src'
-    output_dir = 'build'
-    configureDoxyfile(input_dir, output_dir)
+#    input_dir = '../src'
+    output_dir = '../build'
+#    configureDoxyfile(input_dir, output_dir)
     subprocess.call('doxygen Doxyfile_RTD', shell = True)
     breathe_projects['Connect Four'] = output_dir + '/xml'
